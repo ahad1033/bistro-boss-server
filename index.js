0211732cb9814c8a17567285f895c33c8e25df97
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-11-04 06:41:30 +0600

    make delete functionality for the cart data delete

--- index.js
+++ index.js
@@ -63,6 +63,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/carts/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = { _id: new Object(id) };
+      const result = await cartCollection.delete(query);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
