74d6c5fedced51f2823fead9a77c6e6f6310c9fa
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-11-11 07:38:42 +0600

    bug fixes -78-7

--- index.js
+++ index.js
@@ -36,6 +36,13 @@ async function run() {
     const cartCollection = client.db("bistroDB").collection("carts");
 
 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'})
+
+      res.send({ token })
+    })
+
     //USER DATA
     app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray();
