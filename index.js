ba28dab41b75463c6ddcacdb5504f87721bda543
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-11-11 07:58:06 +0600

    receive jwt token for verification-78-8

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verityJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.this.state(401).send({ error: true, message: 'unauthorized access' });
+  }
+
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.state(401).send({ error: true, message: 'unauthorized access' });
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ub5yn43.mongodb.net/?retryWrites=true&w=majority`;
@@ -38,7 +54,7 @@ async function run() {
 
     app.post('/jwt', (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
 
       res.send({ token })
     })
@@ -87,16 +103,21 @@ async function run() {
     });
 
     //cart collection api
-    app.get('/carts', async (req, res) => {
+    app.get('/carts', verityJWT, async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([])
       }
-      else {
-        const query = { email: email };
-        const result = await cartCollection.find(query).toArray();
-        res.send(result);
+
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({error: true, message: 'forbiden access'});
       }
+
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+
     });
 
     app.post('/carts', async (req, res) => {
