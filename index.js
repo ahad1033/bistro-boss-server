31b25b292f1df8be4e8a89e1e52c267cc946ab1f
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-11-11 10:26:26 +0600

    -78-11-

--- index.js
+++ index.js
@@ -59,8 +59,18 @@ async function run() {
       res.send({ token })
     })
 
+    const verifyAdmin = async (req, res, next ) => {
+      const email = req.decoded.email;
+      const query = { email: email}
+      const user = await usersCollection.findOne(query);
+      if(user?.role !== 'admin') {
+        return res.status(403).send({error: true, message: 'forbidden message'});
+      }
+      next();
+    }
+
     //USER DATA
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
